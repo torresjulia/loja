835422758d07c68d4c25d83a0d517ed33ec2afea
julia.torres <juliadittoro@yahoo.com>
2026-09-14 20:02:05 -0300

    [feat]: cria usuarioEntity e uuid

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,20 +1,27 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
-import { UsuarioRepository } from "./usuario.repository";
-import { CriaUsuarioDTO } from "./dto/CriaUsuario.dto";
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UsuarioRepository } from './usuario.repository';
+import { CriaUsuarioDTO } from './dto/CriaUsuario.dto';
+import { UsuarioEntity } from './usuario.entity';
+import { v4 as uuid } from 'uuid';
 
 @Controller('/usuarios')
 export class UsuarioController {
+  constructor(private usuarioRepository: UsuarioRepository) {}
 
-    constructor (private usuarioRepository: UsuarioRepository) {}
+  @Post()
+  async criaUsuario(@Body() dadosDoUsuario: CriaUsuarioDTO) {
+    const usuarioEntity = new UsuarioEntity();
+    usuarioEntity.email = dadosDoUsuario.email;
+    usuarioEntity.senha = dadosDoUsuario.senha;
+    usuarioEntity.nome = dadosDoUsuario.nome;
+    usuarioEntity.id = uuid();
 
-    @Post()
-    async criaUsuario(@Body() dadosDoUsuario: CriaUsuarioDTO) {
-        this.usuarioRepository.salvar(dadosDoUsuario);
-        return dadosDoUsuario;
-    }
+    this.usuarioRepository.salvar(usuarioEntity);
+    return { id: usuarioEntity.id, message: 'Usuário criado com sucesso' };
+  }
 
-    @Get()
-    async listarUsuarios() {
-        return this.usuarioRepository.listar();
-    }
+  @Get()
+  async listarUsuarios() {
+    return this.usuarioRepository.listar();
+  }
 }
